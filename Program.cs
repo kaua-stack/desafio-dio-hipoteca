59e0813bf16dbe3e3b82850b7e59c8436ea8a5e4
kaua-alves <queroficarrico54@gmail.com>
2025-10-08 13:45:18 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "11998887777", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\n------------------------\n");
+
+            Console.WriteLine("Smartphone iPhone:");
+            Smartphone iphone = new Iphone(numero: "11997776666", modelo: "iPhone 15 Pro", imei: "222222222222222", memoria: 256);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.WriteLine("\nPrograma finalizado.");
+        }
+    }
+}
